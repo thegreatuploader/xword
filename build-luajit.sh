8e468d8c05fe87c376f4f63df00e58b411f94d80
thegreatuploader <34802341+thegreatuploader@users.noreply.github.com>
2025-03-18 19:33:27 -0400

    Update build-luajit.sh

--- build-luajit.sh
+++ build-luajit.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-LUAJIT_VERSION=$1
+LUAJIT_VERSION=2.05
 
 if [[ -z "$LUAJIT_VERSION" ]]; then
   echo "First argument must be set to the LuaJIT version"
@@ -15,23 +15,13 @@ mkdir -p $INSTALL_PATH
 
 echo "Building LuaJIT version $LUAJIT_VERSION"
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  BUILD_COMMAND="make MACOSX_DEPLOYMENT_TARGET=10.10 CC='gcc -arch x86_64'"
-  INSTALL_COMMAND="cp libluajit.a $INSTALL_PATH/lib/liblua51.a"
-  LIBS_DIR="macosx"
-elif [[ "$OSTYPE" == "linux"* ]]; then
-  BUILD_COMMAND="make CC='gcc -fPIC'"
-  INSTALL_COMMAND="cp libluajit.a $INSTALL_PATH/lib/liblua51.a"
-  LIBS_DIR="linux"
-else  # Windows
-  BUILD_COMMAND="./msvcbuild.bat"
+BUILD_COMMAND="./msvcbuild.bat"
   INSTALL_COMMAND="cp lua51.{dll,exp,lib} $INSTALL_PATH/lib/"
   LIBS_DIR="windows"
-fi
 
 # Check to see if the cache directory is empty
 if [ ! -d "$INSTALL_PATH/lib" ]; then
-  curl -fsSL -o LuaJIT-$LUAJIT_VERSION.tar.gz https://luajit.org/download/LuaJIT-2.0.5.tar.gz
+  curl -fsSL -o LuaJIT-$LUAJIT_VERSION.tar.gz https://github.com/LuaJIT/LuaJIT/archive/refs/tags/v2.0.5.tar.gz
   tar -xzf LuaJIT-$LUAJIT_VERSION.tar.gz
   cd LuaJIT-$LUAJIT_VERSION/src
   eval $BUILD_COMMAND
